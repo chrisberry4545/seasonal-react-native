eb0780f86ec6da827ea4da7734e80010f6382442
chrisberry4545 <chrisberry4545@googlemail.com>
2019-02-17 16:37:31 +0000

    close navigation bar on route change

--- src/views/shared/navigation-bar.js
+++ src/views/shared/navigation-bar.js
@@ -4,10 +4,13 @@ import {
   styles
 } from '../../styles';
 import { Ionicons } from '@expo/vector-icons';
+import { NavigationEvents } from 'react-navigation';
 
 export const NavigationBar = ({ navigation }) => {
   return (
     <View style={ styles.cNavigationBar }>
+      <NavigationEvents
+        onDidFocus={() => navigation.closeDrawer()} />
       <TouchableOpacity
         style={ styles.cNavigationBarMenuButton }
         onPress={ navigation.openDrawer }>
