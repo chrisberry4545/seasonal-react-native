7d8aec41f5cb89e39e58755d1d7d307df0f75528
chrisberry4545 <chrisberry4545@googlemail.com>
2019-06-09 16:34:48 +0100

    expo update

--- src/helpers/analytics.ts
+++ src/helpers/analytics.ts
@@ -1,4 +1,4 @@
-import { Amplitude } from 'expo';
+import * as Amplitude from 'expo-analytics-amplitude';
 
 export const initAnalytics = () => {
   Amplitude.initialize('abacf5c432944e007c91bab33fcfe44e');
